2d98b987ec806e26a72251cf90bd9faf0f61384e
charles-tech <charles.fernando.miranda@gmail.com>
2023-02-27 13:50:46 -0300

    Questão 4

---
+++ script.js
@@ -0,0 +1,24 @@
+var sp = 67836.43;
+var rj = 36678.66;
+var mg = 29229.88;
+var es = 27165.48;
+var outro = 19849.53
+
+var total = sp+rj+mg+es+outro;
+console.log(total)
+
+var perSp = (sp/total)*100;
+var perRj = (rj/total)*100;
+var perMg = (mg/total)*100;
+var perEs = (es/total)*100;
+var perOutros = (outro/total)*100;
+
+
+console.log("Porcentagem Sp é: "+ perSp);
+console.log("Porcentagem Rj é: "+ perRj);
+console.log("Porcentagem Mg é: "+ perMg);
+console.log("Porcentagem Es é: "+ perEs);
+console.log("Porcentagem Outros é: "+ perOutros);
+
+
+
